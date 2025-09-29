c9d73b919b3c026ae484a4038cecc9a8234adb8b
Miroslav <38636350+Miroslav2497@users.noreply.github.com>
2025-09-29 17:56:46 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  experimental: {
-    appDir: false,
-  },
   webpack: (config) => {
     config.resolve.fallback = {
       ...config.resolve.fallback,
@@ -13,11 +10,6 @@ const nextConfig = {
       tls: false,
     };
     return config;
-  },
-  env: {
-    NEXT_PUBLIC_CONTRACT_ADDRESS: process.env.NEXT_PUBLIC_CONTRACT_ADDRESS,
-    NEXT_PUBLIC_CHAIN_ID: process.env.NEXT_PUBLIC_CHAIN_ID,
-    NEXT_PUBLIC_RPC_URL: process.env.NEXT_PUBLIC_RPC_URL,
   }
 }
 
