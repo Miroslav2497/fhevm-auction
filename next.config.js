df18461d4ccf963d021360188fa012a4a7539ae2
Miroslav <38636350+Miroslav2497@users.noreply.github.com>
2025-09-29 17:46:26 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -14,6 +14,11 @@ const nextConfig = {
     };
     return config;
   },
+  env: {
+    NEXT_PUBLIC_CONTRACT_ADDRESS: process.env.NEXT_PUBLIC_CONTRACT_ADDRESS,
+    NEXT_PUBLIC_CHAIN_ID: process.env.NEXT_PUBLIC_CHAIN_ID,
+    NEXT_PUBLIC_RPC_URL: process.env.NEXT_PUBLIC_RPC_URL,
+  }
 }
 
 module.exports = nextConfig
